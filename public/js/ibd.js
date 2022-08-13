be6bb1521611191e13e710db1d9e27a0f10a2202
Matthew <mwhiteman83@gmail.com>
2022-08-13 13:46:41 -0600

    moving forward?

--- public/js/ibd.js
+++ public/js/ibd.js
@@ -4,3 +4,11 @@ let db;
 
 const request = indexedDB.open('billZZZ', 1);
 
+request.onupgradeneeded = ({ target }) => {//allows us to add "stores" (collections, tables)
+    let db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+  };
+  //grabs reference to db and targets input
+  request.onsuccess = ({ target }) => {
+    db = target.result;
+  };
