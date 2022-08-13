ac5e00141c3190f91d52fdc6a48c8e1c52570714
Matthew <mwhiteman83@gmail.com>
2022-08-13 14:00:57 -0600

    completed indexed db tested functionality success

--- public/js/ibd.js
+++ public/js/ibd.js
@@ -11,4 +11,45 @@ request.onupgradeneeded = ({ target }) => {//allows us to add "stores" (collecti
   //grabs reference to db and targets input
   request.onsuccess = ({ target }) => {
     db = target.result;
+    // check if app is online before reading from database
+    if (navigator.onLine) {
+      checkDatabase();
+    }
   };
+
+  request.onerror = function(event) {
+    console.log("Holy Cow there is an issue! " + event.target.errorCode);
+  };
+  function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+  }
+  function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    //adds CRUD functionality
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {
+          return response.json();
+        })
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+      }
+    };
+  }
+  window.addEventListener("online", checkDatabase);
